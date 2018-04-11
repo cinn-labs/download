978fe670bad0dae4b1e0005348924b38953cef80
Chris Storch <ccstorch@gmail.com>
2018-04-11 17:28:39 +0100

    Up

--- download.go
+++ download.go
@@ -3,6 +3,7 @@ package download
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -18,12 +19,14 @@ func DownloadFolder() string {
 }
 
 func GenerateFolder(fn string, fe string) (string, error) {
+	ffn := strings.Replace(fn, " ", "_", -1)
+	ffn = strings.Replace(ffn, "#", "", -1)
 	df := DownloadFolder()
 	if _, err := os.Stat(df); os.IsNotExist(err) {
 		os.Mkdir(df, 0700)
 	}
 	folderPath := fmt.Sprintf("%s%s", df, uuid.NewV4().String())
 	err := os.Mkdir(folderPath, 0700)
-	filePath := fmt.Sprintf("%s/%s.%s", folderPath, fn, fe)
+	filePath := fmt.Sprintf("%s/%s.%s", folderPath, ffn, fe)
 	return filePath, err
 }
